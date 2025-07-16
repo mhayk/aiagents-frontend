a6bbbd06e8d7a241dfe42b5c9328a52bca9021f6
Mhayk Whandson <hi@mhayk.com>
2025-07-16 10:52:11 +0100

    feat: configure biome

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './app'
 
+// biome-ignore lint/style/noNull: mandatory by React
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
