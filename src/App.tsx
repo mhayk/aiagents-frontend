05655b36d22912661ec718f846a92e82f63a1f0e
Mhayk Whandson <hi@mhayk.com>
2025-07-23 14:15:45 +0100

    feat: create the initial routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <div>
-      <Button variant="ghost">Hello World</Button>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<CreateRoom />} />
+          <Route path="/room/:roomId" element={<Room />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
